47b405fc264e11b1bd790d01e05cb387f33fdac0
young <sealink272@gmail.com>
2025-09-09 13:26:46 +0800

    Update bitquantum-cli.cpp

--- src/bitquantum-cli.cpp
+++ src/bitquantum-cli.cpp
@@ -1567,7 +1567,7 @@ static void StartLoopMining(const std::string& address, const std::vector<std::s
                                 CScript opret = CScript() << OP_RETURN << commitment_data;
                                 
                                 // Add witness commitment to coinbase
-                                CMutableTransaction coinbase_mtx(block.vtx[0]->nVersion);
+                                CMutableTransaction coinbase_mtx(block.vtx[0]->version);
                                 coinbase_mtx.vin = block.vtx[0]->vin;
                                 coinbase_mtx.vout = block.vtx[0]->vout;
                                 coinbase_mtx.nLockTime = block.vtx[0]->nLockTime;
