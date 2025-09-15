c484861ab53e4f69f91c3860a22bb9c61a178e3f
young <sealink272@gmail.com>
2025-09-15 09:53:10 +0800

    Revert "Update chainparams.cpp"
    This reverts commit 834151bbcc37d8f1dbcf6f2577113e9668ff7c17.

--- src/kernel/chainparams.cpp
+++ src/kernel/chainparams.cpp
@@ -93,7 +93,7 @@ class CMainParams : public CChainParams {
 public:
     CMainParams() {
         m_chain_type = ChainType::MAIN;
-        
+        consensus.strNetworkID = "bitquantum";
         consensus.signet_blocks = false;
         consensus.signet_challenge.clear();
         consensus.nSubsidyHalvingInterval = 210000;
@@ -144,7 +144,7 @@ public:
         m_assumed_chain_state_size = 14;
 
         vSeeds.clear();
-        vFixedSeeds.clear();
+        fixedSeeds.clear();
 
         // Create genesis with nonce 0, then find a valid nonce using RandomQ
         genesis = CreateGenesisBlock(1756857263, 1379716, 0x1e0ffff0, 1, 50 * COIN);
