78060da39658d8fb47f97e077280024899eb4ac2
young <sealink272@gmail.com>
2025-09-24 22:00:53 +0800

    Update gpuminer-opencl.cpp

--- src/tools/gpuminer-opencl.cpp
+++ src/tools/gpuminer-opencl.cpp
@@ -276,7 +276,7 @@ static const char* kOpenCLKernel = R"CLC(
 // Rotate right
 inline uint rotr(uint x, uint n) { return (x >> n) | (x << (32 - n)); }
 
-inline void sha256_transform(const __global uchar* data, uint state[8]) {
+inline void sha256_transform(const uchar* data, uint state[8]) {
     const uint K[64] = {
         0x428a2f98,0x71374491,0xb5c0fbcf,0xe9b5dba5,0x3956c25b,0x59f111f1,0x923f82a4,0xab1c5ed5,
         0xd807aa98,0x12835b01,0x243185be,0x550c7dc3,0x72be5d74,0x80deb1fe,0x9bdc06a7,0xc19bf174,
